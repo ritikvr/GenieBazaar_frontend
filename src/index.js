4e4fbc4107d81ba53128760e6c114aec40e869e5
Ritik <ritik11oct2003@gmail.com>
2024-01-22 19:36:14 +0530

    3 commit

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import axios from 'axios'
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/index";
-
+axios.defaults.baseURL = 'https://genie-bazaar-backend.vercel.app';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
